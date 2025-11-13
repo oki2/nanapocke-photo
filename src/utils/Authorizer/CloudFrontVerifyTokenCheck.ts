61239c57b732cd6d981be24759ba36fe3246b1cc
tomo <ishikawa@uxbrew.jp>
2025-11-13 17:46:21 +0900

    Setting情報の修正

--- src/utils/Authorizer/CloudFrontVerifyTokenCheck.ts
+++ src/utils/Authorizer/CloudFrontVerifyTokenCheck.ts
@@ -1,8 +1,10 @@
-import { Setting } from "./Setting";
+import {Setting} from "../../config";
 export function CloudFrontVerifyTokenCheck(event: any = {}): boolean {
-  const xOriginVerify = event.headers?.['x-origin-verify-token'] || '';
-  if(xOriginVerify !== Setting.X_ORIGIN_VERIFY_TOKEN) {
-    console.log(`xOriginVerify : ${xOriginVerify} / X_ORIGIN_VERIFY_TOKEN : ${Setting.X_ORIGIN_VERIFY_TOKEN}`);
+  const xOriginVerify = event.headers?.["x-origin-verify-token"] || "";
+  if (xOriginVerify !== Setting.X_ORIGIN_VERIFY_TOKEN) {
+    console.log(
+      `xOriginVerify : ${xOriginVerify} / X_ORIGIN_VERIFY_TOKEN : ${Setting.X_ORIGIN_VERIFY_TOKEN}`
+    );
     return false;
   }
   return true;
--- src/utils/Authorizer/Setting.ts
+++
@@ -1,3 +0,0 @@
-export const Setting = {
-  X_ORIGIN_VERIFY_TOKEN : process.env.X_ORIGIN_VERIFY_TOKEN || ''
-};
