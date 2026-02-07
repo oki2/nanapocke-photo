797ab1c6d7b7aa90ab93d0bbb7074b9005fc9d95
tomo <ishikawa@uxbrew.jp>
2026-02-07 21:49:53 +0900

    料金の正式版価格を適用

--- src/config/Price.ts
+++ src/config/Price.ts
@@ -2,40 +2,43 @@ import * as AlbumConfig from "./Model/Album";
 import * as PhotoConfig from "./Model/Photo";
 
 export const PHOTO_PRICE = {
+  // 通常価格
   [AlbumConfig.PRICE_TABLE.BASIC]: {
     [PhotoConfig.PRICE_TIER.STANDARD]: {
-      [PhotoConfig.SALES_SIZE.DL_ORIGINAL]: 101,
-      [PhotoConfig.SALES_SIZE.PRINT_L]: 102,
-      [PhotoConfig.SALES_SIZE.PRINT_2L]: 103,
+      [PhotoConfig.SALES_SIZE.DL_ORIGINAL]: 167,
+      [PhotoConfig.SALES_SIZE.PRINT_L]: 110,
+      [PhotoConfig.SALES_SIZE.PRINT_2L]: 235,
+    },
+    [PhotoConfig.PRICE_TIER.PREMIUM]: {
+      [PhotoConfig.SALES_SIZE.DL_ORIGINAL]: 224,
+      [PhotoConfig.SALES_SIZE.PRINT_L]: 169,
+      [PhotoConfig.SALES_SIZE.PRINT_2L]: 451,
+    },
+  },
+  // 再販価格
+  [AlbumConfig.PRICE_TABLE.PREMIUM]: {
+    [PhotoConfig.PRICE_TIER.STANDARD]: {
+      [PhotoConfig.SALES_SIZE.DL_ORIGINAL]: 250,
+      [PhotoConfig.SALES_SIZE.PRINT_L]: 165,
+      [PhotoConfig.SALES_SIZE.PRINT_2L]: 352,
+    },
+    [PhotoConfig.PRICE_TIER.PREMIUM]: {
+      [PhotoConfig.SALES_SIZE.DL_ORIGINAL]: 336,
+      [PhotoConfig.SALES_SIZE.PRINT_L]: 253,
+      [PhotoConfig.SALES_SIZE.PRINT_2L]: 676,
+    },
+  },
+  // セール価格
+  [AlbumConfig.PRICE_TABLE.SALE]: {
+    [PhotoConfig.PRICE_TIER.STANDARD]: {
+      [PhotoConfig.SALES_SIZE.DL_ORIGINAL]: 150,
+      [PhotoConfig.SALES_SIZE.PRINT_L]: 99,
+      [PhotoConfig.SALES_SIZE.PRINT_2L]: 211,
     },
     [PhotoConfig.PRICE_TIER.PREMIUM]: {
       [PhotoConfig.SALES_SIZE.DL_ORIGINAL]: 201,
-      [PhotoConfig.SALES_SIZE.PRINT_L]: 202,
-      [PhotoConfig.SALES_SIZE.PRINT_2L]: 203,
-    },
-  },
-  [AlbumConfig.PRICE_TABLE.PREMIUM]: {
-    [PhotoConfig.PRICE_TIER.STANDARD]: {
-      [PhotoConfig.SALES_SIZE.DL_ORIGINAL]: 301,
-      [PhotoConfig.SALES_SIZE.PRINT_L]: 302,
-      [PhotoConfig.SALES_SIZE.PRINT_2L]: 303,
-    },
-    [PhotoConfig.PRICE_TIER.PREMIUM]: {
-      [PhotoConfig.SALES_SIZE.DL_ORIGINAL]: 401,
-      [PhotoConfig.SALES_SIZE.PRINT_L]: 402,
-      [PhotoConfig.SALES_SIZE.PRINT_2L]: 403,
-    },
-  },
-  [AlbumConfig.PRICE_TABLE.SALE]: {
-    [PhotoConfig.PRICE_TIER.STANDARD]: {
-      [PhotoConfig.SALES_SIZE.DL_ORIGINAL]: 501,
-      [PhotoConfig.SALES_SIZE.PRINT_L]: 502,
-      [PhotoConfig.SALES_SIZE.PRINT_2L]: 503,
-    },
-    [PhotoConfig.PRICE_TIER.PREMIUM]: {
-      [PhotoConfig.SALES_SIZE.DL_ORIGINAL]: 601,
-      [PhotoConfig.SALES_SIZE.PRINT_L]: 602,
-      [PhotoConfig.SALES_SIZE.PRINT_2L]: 603,
+      [PhotoConfig.SALES_SIZE.PRINT_L]: 152,
+      [PhotoConfig.SALES_SIZE.PRINT_2L]: 405,
     },
   },
 } as const;
